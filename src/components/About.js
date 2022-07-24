f116d449ea68c095f62848666862ed1496b4e817
Maryanne Anyango <mayannanneanyango98@gmail.com>
2022-07-24 12:19:53 +0300

    files

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,22 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
+  // console.log(props.links.linkedin);
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {/* if({props.bio}){
+        <p>{props.bio}</p>
+      }else{
+        null
+      } */}
+
+    {props.bio ? <p>{props.bio}</p> : null}
+
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github = {props.links.github} linkedin = {props.links.linkedin}/>
     </div>
   );
 }
